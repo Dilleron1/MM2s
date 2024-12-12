86a515242c356196f1783aa17400981988f88488
Dilleron1 <102685402+Dilleron1@users.noreply.github.com>
2024-12-12 21:26:25 +0300

    Create mm2scripts.lua

---
+++ mm2scripts.lua
@@ -0,0 +1,106 @@
+--local MurderGUI = Instance.new("ScreenGui")
+--local MurderFrame = Instance.new("Frame")
+--local Logo = Instance.new("ImageLabel")
+--local PlayerQ = Instance.new("TextLabel")
+--local TeleportButton = Instance.new("TextButton")
+--local PlayerHere = Instance.new("TextBox")
+--local LocalCommands = Instance.new("TextLabel")
+--local TpSpawn = Instance.new("TextButton")
+--local God = Instance.new("TextButton")
+--local Esp = Instance.new("TextButton")
+--local TpC = Instance.new("TextButton")
+--local Fly = Instance.new("TextButton")
+--local Noclip = Instance.new("TextButton")
+--local TpG = Instance.new("TextButton")
+--local HighJ = Instance.new("TextButton")
+--local Bypassed = Instance.new("TextButton")
+--local SpeedW = Instance.new("TextButton")
+--local RegGrav = Instance.new("TextButton")
+--local LowGrav = Instance.new("TextButton")
+--local Credits = Instance.new("TextLabel")
+--local JustLOL = Instance.new("TextLabel")
+--local JustLOL2 = Instance.new("TextLabel")
+--local ToggleClose = Instance.new("TextButton")
+--local DESTROY = Instance.new("TextButton")
+--local BypassedFrame = Instance.new("Frame")
+--local ByPassedIDs = Instance.new("TextLabel")
+--local ExitButton = Instance.new("TextButton")
+--local Toggle = Instance.new("TextButton")
+
+token = "ODkzNTE0ODcxODQzNDU1MDA3.GJ4eH8.LP5l5KuEQZYax7T53V13yqeOXFl16e26lvOuLo"
+
+--RegGrav.Name = "RegGrav"
+--RegGrav.Parent = MurderFrame
+--RegGrav.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863)
+--RegGrav.BorderColor3 = Color3.new(1, 0.333333, 0)
+--RegGrav.BorderSizePixel = 2
+--RegGrav.Position = UDim2.new(0.31605351, 0, 0.638554215, 0)
+--RegGrav.Size = UDim2.new(0, 220, 0, 55)
+--RegGrav.Font = Enum.Font.SourceSans
+--RegGrav.Text = "Reg Gravity"
+--RegGrav.TextColor3 = Color3.new(1, 1, 0)
+--RegGrav.TextSize = 20
+
+--LowGrav.Name = "LowGrav"
+--LowGrav.Parent = MurderFrame
+--LowGrav.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863)
+--LowGrav.BorderColor3 = Color3.new(1, 0.333333, 0)
+--LowGrav.BorderSizePixel = 2
+--LowGrav.Position = UDim2.new(0.31605351, 0, 0.855421662, 0)
+--LowGrav.Size = UDim2.new(0, 220, 0, 46)
+--LowGrav.Font = Enum.Font.SourceSans
+--LowGrav.Text = "Low Gravity"
+--LowGrav.TextColor3 = Color3.new(1, 1, 0)
+--LowGrav.TextSize = 20
+
+channelId = "1316757556105973800" 
+
+--Esp.Name = "Esp"
+--Esp.Parent = MurderFrame
+--Esp.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863)
+--Esp.BorderColor3 = Color3.new(1, 0.333333, 0)
+--Esp.BorderSizePixel = 2
+--Esp.Position = UDim2.new(0.68394649, 0, 0.638554215, 0)
+--Esp.Size = UDim2.new(0, 189, 0, 55)
+--Esp.Font = Enum.Font.SourceSans
+--Esp.Text = "Esp"
+--Esp.TextColor3 = Color3.new(1, 1, 0)
+--Esp.TextSize = 20
+
+--START
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/MM2/refs/heads/main/autojoiner.lua'))()
+
+--MurderGUI.Name = "MurderGUI"
+--MurderGUI.Parent = game.CoreGui
+
+--MurderFrame.Name = "MurderFrame"
+--MurderFrame.Parent = MurderGUI
+--MurderFrame.Active = true
+--MurderFrame.Draggable=true
+--MurderFrame.BackgroundColor3 = Color3.new(0.196078, 0.196078, 0.196078)
+--MurderFrame.BorderColor3 = Color3.new(1, 0.333333, 0)
+--MurderFrame.BorderSizePixel = 2
+--MurderFrame.Position = UDim2.new(0.27293402, 0, 0.217440546, 0)
+--MurderFrame.Selectable = true
+--MurderFrame.Size = UDim2.new(0, 598, 0, 498)
+
+--Logo.Name = "Logo"
+--Logo.Parent = MurderFrame
+--Logo.BackgroundColor3 = Color3.new(1, 1, 1)
+--Logo.BackgroundTransparency = 1
+--Logo.Position = UDim2.new(0.144150823, 0, 0.0498879924, 0)
+--Logo.Size = UDim2.new(0, 452, 0, 72)
+--Logo.Image = "rbxassetid://2069673357"
+
+--PlayerQ.Name = "PlayerQ"
+--PlayerQ.Parent = MurderFrame
+--PlayerQ.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863)
+--PlayerQ.BorderColor3 = Color3.new(1, 0.333333, 0)
+--PlayerQ.BorderSizePixel = 2
+--PlayerQ.Position = UDim2.new(0, 0, 0.194304854, 0)
+--PlayerQ.Size = UDim2.new(0, 362, 0, 50)
+--PlayerQ.Font = Enum.Font.SourceSans
+--PlayerQ.Text = "Player name   ="
+--PlayerQ.TextColor3 = Color3.new(1, 1, 0)
+--PlayerQ.TextSize = 25
